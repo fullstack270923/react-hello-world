536972bba1e846c0217d99c7a92e4ca95fad0fe5
fullstack290623 <fullstack290623@gmail.com>
2024-04-14 20:15:53 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+class App extends React.Component {
+  // data
+  state = {
+    name: 'Danny',
+    age: 30
+  }
 
-        <h1>Hello world!!***</h1>
-        
-      </header>
-    </div>
-  );
+  // redner
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Hello {this.state.name}</h1>
+          <h1>AGE = {this.state.age}</h1>
+
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
