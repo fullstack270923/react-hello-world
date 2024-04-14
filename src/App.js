0e8ed73a527ad9cfce95dba3886f84f34dd6d8d0
fullstack290623 <fullstack290623@gmail.com>
2024-04-14 20:25:47 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends React.Component {
-  // data
-  state = {
-    name: 'Danny',
-    age: 30
-  }
 
-  // redner
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1>Hello {this.state.name}</h1>
-          <h1>AGE = {this.state.age}</h1>
+    // data
+    state = {
+      name: 'Danny',
+      age: 30
+    }
 
-        </header>
-      </div>
-    );
-  }
+    // redner
+    render() {
+      //let x 
+      //this.x // error
+      return (
+        <div className="App">
+          <header className="App-header">
+            <h1>Hello {this.state.name}</h1>
+            <h1>AGE = {this.state.age}</h1>
+
+          </header>
+        </div>
+      );
+    }
 }
 
 export default App;
